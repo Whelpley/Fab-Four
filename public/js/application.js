c13e138c55f9cec4bc13ff06593b3404f6171080
Beni Shpringer <email": null>
2016-01-28 13:51:47 -0800

    Adds fixable function

--- public/js/application.js
+++ public/js/application.js
@@ -1,22 +1,58 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  // populateCells();
+  fillAColumnListener();
+
+
 });
 
-$('#column_id1').submit(function(e){
-  e.preventDefault;
-  $.ajax({
-    method: "POST",
-    url: '/new_cell'
-  })
-  .done(function(responseData){
-    (responseData.selector).css("background-color", responseData.color)
-  })
-})
 
+
+function colorChooser(){
+  var color = false;
+        if (this.color == true) {
+          this.color = false;
+          return "red"
+        }
+        else {
+          this.color = true;
+          return "black"
+        }
+}
+
+// Look @ the cellArray and parse out only elements that have common column ID #.
+// First check the lowest rowID for filled = false.  If false, change value to true.
+// If filled is already true, look to the next lowest rowID# and perform above operation.
+
+
+var fillAColumnListener = function() {
+$('#column_id0').on('click', function(e){
+  e.preventDefault();
+  console.log('clicking');
+  var column0Cells = cellArray.filter(function(cell){
+    return cell.column == 0;
+  })
+  for (i=0; i<6; i++){
+    if (column0Cells[i].filled == false){
+      column0Cells[i].filled = true
+      column0Cells[i].color = colorChooser();
+      break
+    }
+  }
+    console.log(column0Cells)
+})
+}
+
+// $('#column_id1').submit(function(e){
+//   e.preventDefault;
+//   $.ajax({
+//     method: "POST",
+//     url: '/new_cell'
+//   })
+//   .done(function(responseData){
+//     (responseData.selector).css("background-color", responseData.color)
+//   })
+// })
 
 
 var cellArray = [];
@@ -36,9 +72,8 @@ populateCells = function(){
     newCell.row = Math.floor(i/7);
     newCell.column = i%7;
     cellArray.push(newCell)
+    }
   }
-}
-
 
 
 
