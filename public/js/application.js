70dc437e42f543c8a68cd3907388d3f69208d2b8
Beni Shpringer <email": null>
2016-01-28 12:05:12 -0800

    creates view and populates 42 cells

--- public/js/application.js
+++ public/js/application.js
@@ -5,3 +5,40 @@ $(document).ready(function() {
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
+
+$('#column_id1').submit(function(e){
+  e.preventDefault;
+  $.ajax({
+    method: "POST",
+    url: '/new_cell'
+  })
+  .done(function(responseData){
+    (responseData.selector).css("background-color", responseData.color)
+  })
+})
+
+
+
+var cellArray = [];
+
+cell = function(){
+  this.row = null,
+  this.column = null,
+  this.filled = false,
+  this.color = null,
+  this.id = null
+}
+
+populateCells = function(){
+  for (i=0; i < 42; i++){
+    var newCell = new cell;
+    newCell.id = i;
+    newCell.row = Math.floor(i/7);
+    newCell.column = i%7;
+    cellArray.push(newCell)
+  }
+}
+
+
+
+
