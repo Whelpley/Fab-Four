72870190630640a5a1d61f672b986dcdb79efaf2
Beni Shpringer <email": null>
2016-01-28 16:16:59 -0800

    Update View with Cats and Dogs and Logic

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  // populateCells();
+  populateCells();
   fillAColumnListener();
 
 
@@ -9,14 +9,14 @@ $(document).ready(function() {
 
 
 function colorChooser(){
-  var color = false;
+  var color = true;
         if (this.color == true) {
           this.color = false;
-          return "red"
+          return "<img src = 'http://assets.nydailynews.com/polopoly_fs/1.1245686!/img/httpImage/image.jpg_gen/derivatives/article_970/afp-cute-puppy.jpg' height=100px width=100px>"
         }
         else {
           this.color = true;
-          return "black"
+          return "<img src = 'https://openclipart.org/download/231967/blackcat.svg' height=100px width=100px>"
         }
 }
 
@@ -25,21 +25,34 @@ function colorChooser(){
 // If filled is already true, look to the next lowest rowID# and perform above operation.
 
 
+
+
+
+
 var fillAColumnListener = function() {
-$('#column_id0').on('click', function(e){
+$('.column_class').click(function(e){
+  var location = $(this);
+  console.log(location);
   e.preventDefault();
   console.log('clicking');
-  var column0Cells = cellArray.filter(function(cell){
-    return cell.column == 0;
-  })
+  var columnCells = cellArray.filter(function(cell){
+    return cell.column == location.attr('id')
+  }) // the above is a filter function returning only the cells within the column matching location.attr(id)
   for (i=0; i<6; i++){
-    if (column0Cells[i].filled == false){
-      column0Cells[i].filled = true
-      column0Cells[i].color = colorChooser();
+    if (columnCells[5].filled == true) {
+        alert ("THIS COLUMN IS FULL! PICK A DIFFERENT ONE.");
+      break
+    }
+    else if (columnCells[i].filled == false){
+      columnCells[i].filled = true;
+      columnCells[i].color = colorChooser();
+      var cellColor = columnCells[i].color;
+      var cellRow = columnCells[i].row;
+      var cellColumn = columnCells[i].column + 1;
+      $("#row_"+cellRow+" td:nth-child("+ cellColumn + ")").prepend(cellColor);
       break
     }
   }
-    console.log(column0Cells)
 })
 }
 
